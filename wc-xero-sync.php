59a72c0826bfc975f74c90450d2797f161bfed5e
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-12-09 09:00:51 +1000

    Initial Commit

---
+++ wc-xero-sync.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* Plugin Name: ACL WooCommerce Xero Sync
+* Plugin URI:  https://askcharlyleetham.com
+* Description: Multifunction sync WooCommerce with Xero
+* Version: 1.0.0
+* Author: Charly Leetham
+* Author URI:  https://askcharlyleetham.com
+* License:     GPL-2.0+
+* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+* Text Domain: acl-wc-xero-sync
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+// Include Composer autoloader
+require_once __DIR__ . '/vendor/autoload.php';
